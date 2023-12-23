9ae222eb25eb6b7aaadf5a92bea9377d8404e067
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-23 18:51:45 -0300

    project structure: added folder types and task type, mocks added

---
+++ src/app/lib/mocks/tasks.ts
@@ -0,0 +1,13 @@
+// src/app/lib/mocks
+import { task } from "../types/task";
+
+const tasks: task[] = [
+  {
+    id: '1',
+    title: 'Arrumar a cozinha',
+    description: 'Louça e chão',
+    finished: false,
+    urgency: 4,
+    importance: 8
+  }
+]
---
+++ src/app/lib/types/task.ts
@@ -0,0 +1,9 @@
+// src/app/lib/types
+export type task = {
+  id: string;
+  title: string;
+  description: string;
+  finished: boolean;
+  urgency: number;
+  importance: number;
+}
