e4efa0efbf3bd0b4c9a79b068650173ef3a9a1f7
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-23 13:32:15 -0300

    refactor: enhanced HTML

--- src/app/app/tasks/add/page.tsx
+++ src/app/app/tasks/add/page.tsx
@@ -12,7 +12,13 @@ export default function Page() {
       <h2 className="text-4xl font-extrabold">Adding Task</h2>
       <form className="lg:w-full w-80 m-auto">
         <Label title="Title" styles={labelStyle} />
-        <input type="text" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Task title" required />
+        <input
+          type="text"
+          id="title"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Task title"
+          required
+        />
         
         <Label title="Description" styles={labelStyle} />
         <textarea
@@ -20,6 +26,7 @@ export default function Page() {
           id="description"
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Write your description here..."
+          required
         />
 
         <Label title="Schedule" styles={labelStyle} />
@@ -30,12 +37,17 @@ export default function Page() {
           placeholder="Select date"
           min="2023-12-01T00:00"
           max="2025-12-01T00:00"
+          required
         />
 
         <div className="w-full flex mb-1.5">
           <div className="grow mr-2">
             <Label title="Tag" styles={labelStyle} />
-            <select id="tag" className="w-full h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select
+              id="tag"
+              className="w-full h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            >
               <option defaultValue={"Generic"}>Choose a tag</option>
               <option value="Home">Casa</option>
               <option value="Work">Trabalho</option>
@@ -70,7 +82,7 @@ export default function Page() {
             />
           </div>
         </div>
-        <button type="button" className="w-full mt-2.5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="submit" className="w-full mt-2.5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           <PlusCircleIcon className="w-6 mr-2"/>
           Add Task
         </button>
