4925894da8280339840d6f9490f9f7757851290d
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-27 18:34:32 -0300

    feat: login finished

--- src/app/lib/utils/api/post.ts
+++ src/app/lib/utils/api/post.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
 import api from './api';
 import { TaskFromApi, TaskPayload } from '../../types/task';
+import { LoginData } from '../../types/login';
+import { UserFromApi } from '../../types/user';
 
 async function postTask(task: TaskPayload): Promise<TaskFromApi> {
   try {
@@ -18,6 +20,24 @@ async function postTask(task: TaskPayload): Promise<TaskFromApi> {
   }
 };
 
+async function postLogin(loginData: LoginData): Promise<UserFromApi> {
+  try {
+    const response: AxiosResponse<UserFromApi> = await api.post('users/login', loginData);
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError: AxiosError = error;
+      console.error('Axios error:', axiosError.response?.data || axiosError.message);
+    } else {
+      console.error('Unexpected error:', error);
+    }
+    // You might want to throw or handle the error based on your application's requirements
+    throw error;
+  }
+}
+
 export {
-  postTask
+  postTask,
+  postLogin,
 }
--- src/app/ui/login/LoginForm.tsx
+++ src/app/ui/login/LoginForm.tsx
@@ -1,8 +1,31 @@
+'use client';
+import { LoginData } from "@/app/lib/types/login";
+import { useAuth } from "@/app/lib/utils/context/AuthContext";
 import { ArrowRightEndOnRectangleIcon, AtSymbolIcon, KeyIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function LoginForm() {
+  const [loginData, setLoginData] = useState<Partial<LoginData>>({});
+
+  const handleFormChange = useDebouncedCallback(
+    ({ value, id }: { value: string, id: string}) => {
+      setLoginData({ ...loginData, [id]: value });
+    }, 200);
+
+  const router = useRouter();
+  const { login } = useAuth();
+  const handleLogin = () => {
+    login(loginData as LoginData);
+
+    router.push('/app');
+  }
+
   return (
-    <form className="w-[310px] bg-gray-700/[0.2] backdrop-blur-sm p-7 rounded-3xl md:mt-28 shadow-inner border border-gray-200/[0.4]">
+    <form
+      className="w-[310px] bg-gray-700/[0.2] backdrop-blur-sm p-7 rounded-3xl md:mt-28 shadow-inner border border-gray-200/[0.4]"
+    >
       <div className="flex-1">
         <h1 className="text-xl font-extrabold text-gray-200">
           Sign in to manage your time like a god.
@@ -23,6 +46,7 @@ export default function LoginForm() {
                 name="email"
                 placeholder="Enter your email address"
                 required
+                onChange={(input) => handleFormChange(input.target)}
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -37,18 +61,22 @@ export default function LoginForm() {
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border-2 border-gray-400/[0.2] py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
+                id="passwordHash"
                 type="password"
                 name="password"
                 placeholder="Enter password"
                 required
-                minLength={6}
+                onChange={(input) => handleFormChange(input.target)}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
-        <button className="mt-5 w-full py-2 px-4 flex items-center justify-center bg-gray-500 hover:bg-gray-400 text-white font-bold rounded ring-gray-200/[0.1] hover:ring-4">
+        <button
+          className="mt-5 w-full py-2 px-4 flex items-center justify-center bg-gray-500 hover:bg-gray-400 text-white font-bold rounded ring-gray-200/[0.1] hover:ring-4"
+          onClick={() => handleLogin()}
+          type="button"
+        >
           Log in <ArrowRightEndOnRectangleIcon className="h-5 w-5 ml-1 text-gray-50 inline-block" />
         </button>
       </div>
