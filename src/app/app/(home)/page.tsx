45f65885c42cac02cfb88d697dd822a1cff26506
Lucas Koyama <lucaskoyamahhh@gmail.com>
2024-01-05 00:16:18 -0300

    refactor: not using anymore DateNames class

--- src/app/app/(home)/page.tsx
+++ src/app/app/(home)/page.tsx
@@ -1,8 +1,8 @@
-import { DateNames } from "@/app/lib/utils/DateNames"
 import CategorizedTasksLists from "@/app/ui/tasks/CategorizedTasksLists";
 
 export default function Page() {
-  const today = new DateNames(new Date());
+  const today = new Date();
+  const [weekDay, month, day] = today.toDateString().split(' ');
 
   return (
     <section>
@@ -10,10 +10,10 @@ export default function Page() {
         <h2 className="text-5xl font-extrabold text-blue-950 drop-shadow-2xl">Today</h2>
         <section className="flex items-end mt-2.5 mb-1.5">
           <h2 className="text-4xl font-extrabold text-blue-950 drop-shadow-2xl mr-2.5">
-            {today.getDayOfWeekName().slice(0, 3)}
+            {weekDay}
           </h2>
           <h3 className="text-2xl font-bold text-gray-500 drop-shadow-2xl">
-            {`${today.getDayOfMonth()} ${today.getMonthName().slice(0, 3)}`}
+            {day} {month}
           </h3>
         </section>
       </header>
